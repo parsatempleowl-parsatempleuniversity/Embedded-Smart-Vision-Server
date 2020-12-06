baf50ac216da7217c3daee27d2907364565d22ed
Parsa Temple Owl <tug81042@temple.edu>
2020-12-06 15:03:18 -0500

    Update user_profile.py

--- application/Blueprints/UserProfile/user_profile.py
+++ application/Blueprints/UserProfile/user_profile.py
@@ -7,3 +7,7 @@ user_profile = Blueprint('user_profile', __name__, template_folder='templates')
 @user_profile.route('/userprofile')
 def show_user_profile():
 	return render_template('user_profile.html', current_page='user_profile')
+
+@user_profile.route('/userprofile1')
+def show_user_profile_1():
+	return render_template('user_profile_1.html', current_page='user_profile_1')
